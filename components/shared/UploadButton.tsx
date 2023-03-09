5ee7102d6b42b47ca957c3a4ad97860758a855ac
Kim Jin-Hwan <beliemun@paran.com>
2023-03-09 16:13:23 +0900

    Added Stream Model

--- components/shared/UploadButton.tsx
+++ components/shared/UploadButton.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 interface UploadButton {
-  type: "Product" | "Commmunity";
+  type: "Product" | "Commmunity" | "Stream";
   url: string;
 }
 
@@ -20,12 +20,7 @@ const UploadButton = ({ type, url }: UploadButton) => {
           stroke="currentColor"
           aria-hidden="true"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
         </svg>
       ) : null}
       {type === "Commmunity" ? (
@@ -42,6 +37,21 @@ const UploadButton = ({ type, url }: UploadButton) => {
             strokeWidth="2"
             d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
           ></path>
+        </svg>
+      ) : null}
+      {type === "Stream" ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+          />
         </svg>
       ) : null}
     </Link>
--- pages/stream/index.tsx
+++ pages/stream/index.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "@components/shared";
+import { Layout, UploadButton } from "@components/shared";
 import { useUser } from "@libs/client";
 import { NextPage } from "next";
 import Link from "next/link";
@@ -16,24 +16,7 @@ const Stream: NextPage = () => {
             </div>
           ))}
         </div>
-        <Link
-          href={"/stream/create"}
-          className="fixed bottom-8 right-8 bg-orange-400 hover:bg-orange-500 t-300 rounded-full p-3 text-white shadow-black/20 shadow-xl"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-            />
-          </svg>
-        </Link>
+        <UploadButton type="Stream" url="stream/create" />
       </div>
     </Layout>
   );
