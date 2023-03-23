3b20c6357632e9635b24634a2158bf44edf47686
Kim Jin-Hwan <beliemun@paran.com>
2023-03-23 13:03:47 +0900

    #19.1 Responses and Redirections

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -9,7 +9,7 @@ declare module "iron-session" {
 }
 
 const cookieOptions = {
-  cookieName: "carrotSession",
+  cookieName: "carrot-session",
   password: process.env.IRON_SESSION!,
 };
 
--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,33 @@
-import { NextRequest, userAgent } from "next/server";
+import { getIronSession } from "iron-session/edge";
+import { NextFetchEvent, NextRequest, NextResponse, userAgent } from "next/server";
 
-export const middleware = (req: NextRequest) => {
-  if (req.nextUrl.pathname.startsWith("/")) {
-    const ua = userAgent(req);
-    if (ua.isBot) {
-      return new Response("A bot can not access!", { status: 403 });
+export const middleware = async (req: NextRequest, ev: NextFetchEvent) => {
+  const res = NextResponse.next();
+  const session = await getIronSession(req, res, {
+    cookieName: "carrot-session",
+    password: process.env.IRON_SESSION!,
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production", // if you are using https
+    },
+  });
+
+  if (!req.nextUrl.pathname.startsWith("/_next")) {
+    if (req.nextUrl.pathname.startsWith("/")) {
+      if (userAgent(req).isBot) {
+        return new Response("A bot can not access!", { status: 403 });
+      }
     }
-    console.log(typeof req.cookies);
-  }
-  if (req.nextUrl.pathname.startsWith("/products")) {
-    console.log("Products Middleware");
+
+    if (!req.nextUrl.pathname.startsWith("/api")) {
+      if (!req.nextUrl.pathname.startsWith("/enter") && !session.user) {
+        const url = req.nextUrl.clone();
+        url.pathname = "/enter";
+        return NextResponse.redirect(url);
+      }
+    }
+
+    if (req.nextUrl.pathname.startsWith("/products")) {
+      console.log("Products Middleware");
+    }
   }
 };
